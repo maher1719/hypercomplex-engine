1922460d4b97054a4c892b06f9304e2376f868a7
Maher ben abdessalem <ba.maher94@gmail.com>
2026-09-18 16:16:57 +0100

    further work

--- hypercomplex/printer/CDTablePrinter.py
+++ hypercomplex/printer/CDTablePrinter.py
@@ -67,3 +67,4 @@ class CDTablePrinter:
                         f.write(line + "\n")
             else:
                 raise ValueError("csv_mode must be 'matrix' or 'long'")
+        print(f"File succufuly generated with path: {path}")
